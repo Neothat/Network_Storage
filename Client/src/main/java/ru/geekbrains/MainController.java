ed28ea1b146fb77b61e32da662fc0780283967c3
Denis <neothat@mail.ru>
2021-08-10 06:02:48 +0300

    The appearance of the program is finished

--- Client/src/main/java/ru/geekbrains/MainController.java
+++ Client/src/main/java/ru/geekbrains/MainController.java
@@ -1,14 +1,15 @@
 package ru.geekbrains;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
-
+    public VBox leftPanel, rightPanel;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    public Button copyButton, moveButton, removeButton;
+
+
 }
