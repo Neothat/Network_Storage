b7e87f13d5256d84db3269d1f30fe6be82ee9c2f
Denis <neothat@mail.ru>
2021-08-31 06:20:37 +0300

    Network appeared

--- Client/src/main/java/ru/geekbrains/MainController.java
+++ Client/src/main/java/ru/geekbrains/MainController.java
@@ -1,15 +1,36 @@
 package ru.geekbrains;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    private Network network;
+
     @FXML
     public VBox leftPanel, rightPanel;
     @FXML
     public Button copyButton, moveButton, removeButton;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        network = new Network();
+    }
 
+
+    public void buttonAction(ActionEvent actionEvent) {
+        network.sendMessage("");
+    }
+
+    public void btnExitAction() {
+        Platform.exit();
+    }
 }
---
+++ Client/src/main/java/ru/geekbrains/Network.java
@@ -0,0 +1,45 @@
+package ru.geekbrains;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class Network {
+    private SocketChannel channel;
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 8189;
+
+    public Network() {
+        new Thread(() -> {
+            EventLoopGroup workerGroup = new NioEventLoopGroup();
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            channel = socketChannel;
+                            socketChannel.pipeline().addLast(new StringDecoder());
+                        }
+                    });
+            try {
+                ChannelFuture future = b.connect(HOST, PORT).sync();
+                future.channel().closeFuture().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                workerGroup.shutdownGracefully();
+            }
+        }).start();
+    }
+
+    public void sendMessage(String str) {
+        channel.writeAndFlush(str);
+    }
+}
