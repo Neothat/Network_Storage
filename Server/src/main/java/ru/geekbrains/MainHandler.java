868e6e85e4775d364f56015856dd3ff51df05238
Denis <neothat@mail.ru>
2021-08-31 06:19:25 +0300

    Raised and configured the server

---
+++ Server/src/main/java/ru/geekbrains/MainHandler.java
@@ -0,0 +1,28 @@
+package ru.geekbrains;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        while (buf.readableBytes() > 0) {
+            System.out.println((char) buf.readByte());
+        }
+        buf.release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+
+}
